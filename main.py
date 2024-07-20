d4420846c8a1936a3eac54942c438afa6aef11be
Sasha <2323@vntu.edu.ua>
2024-07-20 19:52:03 +0300

    ...

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from random import randint
+from random import randint, uniform
 
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -22,9 +22,10 @@ clock = pygame.time.Clock()
 
 pygame.mixer.init()
 pygame.mixer.music.load('space.ogg')
-pygame.mixer.music.play()
+# pygame.mixer.music.play()
 
 ship = GameSprite("rocket.png", 5, 400, 80, 100, 10)
+enemies = [GameSprite("ufo.png", randint(0, 620), -50, 80, 50, uniform(1, 3)) for i in range(5)]
 
 while game:
     for event in pygame.event.get():
@@ -38,6 +39,14 @@ while game:
         ship.rect.x -= ship.speed
     if keys[pygame.K_RIGHT] and ship.rect.x < 620:
         ship.rect.x += ship.speed
+
+    for enemy in enemies:
+        enemy.draw()
+        enemy.rect.y += enemy.speed
+        if enemy.rect.y > height:
+            enemy.rect.y = -50
+            enemy.rect.x = randint(0, 620)
+            enemy.speed = uniform(1, 3)
 
     pygame.display.update()
     clock.tick(60)
