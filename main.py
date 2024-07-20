d42b07e298b907a538cf25bb58b8239383dd1fd0
Sasha <2323@vntu.edu.ua>
2024-07-20 14:25:50 +0300

    .

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -11,20 +12,7 @@ class GameSprite(pygame.sprite.Sprite):
     def draw(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-class Player(GameSprite):
-    def update(self):
-        self.draw()
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[pygame.K_RIGHT] and self.rect.x < 620:
-            self.rect.x += self.speed
-    def fire(self):
-        pass
-
-
 width, height = 700, 500
-
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Shooter")
 background = pygame.transform.scale(pygame.image.load("galaxy.jpg"), (width, height))
@@ -36,13 +24,20 @@ pygame.mixer.init()
 pygame.mixer.music.load('space.ogg')
 pygame.mixer.music.play()
 
-ship = Player("rocket.png", 5, 400, 80, 100, 20)
+ship = GameSprite("rocket.png", 5, 400, 80, 100, 10)
 
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
     window.blit(background, (0, 0))
-    ship.update()
+
+    ship.draw()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and ship.rect.x > 5:
+        ship.rect.x -= ship.speed
+    if keys[pygame.K_RIGHT] and ship.rect.x < 620:
+        ship.rect.x += ship.speed
+
     pygame.display.update()
     clock.tick(60)
