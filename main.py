82fcd28175d1607cc3aa8192694fbc45779dc23c
olesatthewheel69 <145124709+olesatthewheel69@users.noreply.github.com>
2024-07-20 13:37:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import pygame
+
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def draw(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        self.draw()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT] and self.rect.x < 620:
+            self.rect.x += self.speed
+    def fire(self):
+        pass
+
+
+width, height = 700, 500
+
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Shooter")
+background = pygame.transform.scale(pygame.image.load("galaxy.jpg"), (width, height))
+
+game = True
+clock = pygame.time.Clock()
+
+pygame.mixer.init()
+pygame.mixer.music.load('space.ogg')
+pygame.mixer.music.play()
+
+ship = Player("rocket.png", 5, 400, 80, 100, 10)
+
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+    window.blit(background, (0, 0))
+    ship.update()
+    pygame.display.update()
+    clock.tick(60)
