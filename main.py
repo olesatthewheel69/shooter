62899359bee13a16fc9aa457bc53bacc2529ba8d
Sasha <2323@vntu.edu.ua>
2024-07-27 17:38:24 +0300

    .

--- main.py
+++ main.py
@@ -21,27 +21,24 @@ game = True
 clock = pygame.time.Clock()
 
 pygame.mixer.init()
-pygame.mixer.music.load("space.ogg")
+pygame.mixer.music.load('space.ogg')
 # pygame.mixer.music.play()
-shot = pygame.mixer.Sound("fire.ogg")
 
-ship = GameSprite("rocket.png", 5, 400, 70, 100, 10)
+ship = GameSprite("rocket.png", 5, 400, 80, 100, 10)
 enemies = [GameSprite("ufo.png", randint(0, 620), -50, 80, 50, randint(1, 3)) for i in range(5)]
 bullets = []
-missed, killed = 0, 0
 
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                bullets.append(GameSprite("bullet.png", ship.rect.centerx - 7, ship.rect.top, 15, 20, -15))
-                shot.play()
     window.blit(background, (0, 0))
 
     ship.draw()
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        b = GameSprite("bullet.png", ship.rect.centerx, ship.rect.y, 5, 20, -4)
+        bullets.append(b)
     if keys[pygame.K_LEFT] and ship.rect.x > 5:
         ship.rect.x -= ship.speed
     if keys[pygame.K_RIGHT] and ship.rect.x < 620:
@@ -54,23 +51,17 @@ while game:
             enemy.rect.y = -50
             enemy.rect.x = randint(0, 620)
             enemy.speed = randint(1, 3)
-            missed += 1
-        elif enemy.rect.colliderect(ship.rect):
-            game = False
 
     for bullet in bullets:
         bullet.draw()
         bullet.rect.y += bullet.speed
-        if bullet.rect.y < 0:
+        if bullet.rect.y < -50:
             bullets.remove(bullet)
         for enemy in enemies:
             if bullet.rect.colliderect(enemy.rect):
                 enemy.rect.y = -50
                 enemy.rect.x = randint(0, 620)
                 enemy.speed = randint(1, 3)
-                if bullet in bullets:
-                    bullets.remove(bullet)
-                killed += 1
-
+    
     pygame.display.update()
     clock.tick(60)
